f93727edba89905e57bb71893ae115cb3ee53790
SprialCloud <lxy990506@gmail.com>
2024-12-06 23:45:55 -0500

    modified

--- Kanbas/Courses/route.js
+++ Kanbas/Courses/route.js
@@ -1,6 +1,8 @@
 import * as dao from "./dao.js";
 import * as modulesDao from "../Modules/dao.js";
 import * as assignmentsDao from "../Assignments/dao.js";
+import * as enrollmentsDao from "../Enrollments/dao.js";
+
 
 export default function CourseRoutes(app) {
 app.get("/api/courses", async (req, res) => {
@@ -10,6 +12,10 @@ res.send(courses);
 
 app.post("/api/courses", async (req, res) => {
 const course = await dao.createCourse(req.body);
+const currentUser = req.session["currentUser"];
+if (currentUser) {
+await enrollmentsDao.enrollUserInCourse(currentUser._id, course._id);
+}
 res.json(course);
 });
 
@@ -64,4 +70,12 @@ app.post("/api/courses/:courseId/assignments/new", (req, res) => {
   const newAssignment = assignmentsDao.createAssignment(assignment);
   res.send(newAssignment);
 });
+
+const findUsersForCourse = async (req, res) => {
+  const { cid } = req.params;
+  const users = await enrollmentsDao.findUsersForCourse(cid);
+  res.json(users);
+  };
+  app.get("/api/courses/:cid/users", findUsersForCourse);
+  
 }
